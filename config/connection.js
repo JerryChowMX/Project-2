cd2918292073aa2d201acd20cb39167190a5240d
jeborunda <joseborunda@gmail.com>
2024-01-02 20:30:42 -0600

    folders and json packages

---
+++ config/connection.js
@@ -0,0 +1,38 @@
+const Sequelize = require("sequelize");
+require("dotenv").config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    "heroku_473ebcf5c039ced",
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "us-cdbr-east-04.cleardb.com",
+      dialect: "mysql",
+      port: 1003,
+    }
+  );
+}
+const Sequelize = require("sequelize");
+require("dotenv").config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    "heroku_473ebcf5c039ced",
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "us-cdbr-east-04.cleardb.com",
+      dialect: "mysql",
+      port: 1003,
+    }
+  );
+}
